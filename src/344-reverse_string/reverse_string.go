357e7ad9155fb002b9b6ace4e312b394b29f357b
DeadWish <2645017681@qq.com>
2017-01-08 17:39:06 +0800

    tab to space

--- src/344-reverse_string/reverse_string.go
+++ src/344-reverse_string/reverse_string.go
@@ -1,8 +1,8 @@
 func reverseString(s string) string {
-	st := []byte(s)
-	slen := len(st)
-	for s, e := 0, slen-1; s < e; s, e = s+1, e-1 {
-		st[s], st[e] = st[e], st[s]
-	}
-	return string(st)
+    st := []byte(s)
+    slen := len(st)
+    for s, e := 0, slen-1; s < e; s, e = s+1, e-1 {
+        st[s], st[e] = st[e], st[s]
+    }
+    return string(st)
 }
