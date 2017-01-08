56c10ec584dc5cd5e427ab4c1cfe53912234cd05
DeadWish <2645017681@qq.com>
2017-01-08 16:46:47 +0800

    all tab switch to space

--- src/1-two_sum/two_sum.go
+++ src/1-two_sum/two_sum.go
@@ -1,12 +1,12 @@
 func twoSum(nums []int, target int) []int {
-	table := make(map[int]int)
-	for i,v := range nums {
-		complement := target - v
-		if index, ok := table[complement]; ok {
-			return []int{index, i}
-		} else {
-			table[v] = i
-		}
-	}
-	return []int{-1, -1}
+    table := make(map[int]int)
+    for i,v := range nums {
+        complement := target - v
+        if index, ok := table[complement]; ok {
+            return []int{index, i}
+        } else {
+            table[v] = i
+        }
+    }
+    return []int{-1, -1}
 }
